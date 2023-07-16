722569746d5cf7863dc09eb91ca39ed4c2d67c6a
冠逸 <yaj55ushydna@gmail.com>
2023-07-16 14:42:31 +0800

    intro section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@
 function App() {
   
   return (
-    <>
-      {/* <div className="w-screen h-screen bg-red">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div> */}
-    </>
+    <div className='px-4 py-20'>
+      <article className='text-white text-center lg:text-left'>
+        <h1 className="font-bold text-3xl lg:text-5xl">Learn to code by watching others</h1>
+        <p className="text-base mt-4 lg:mt-8">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </article>
+
+      <section></section>
+    </div>
   )
 }
 
